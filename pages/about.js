15ee945572732d2d27a60e7a0f9eea0a8013220f
Joseph Ramos <jardix22@gmail.com>
2017-08-09 10:32:47 -0500

    Added pages.

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    <p>This is about us.</p>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const Index = () => (
+  <div>
+    <Link href="/about">
+      <a>About Page</a>
+    </Link>
+    <p>Hello my friend :D!</p>
+  </div>
+)
+
+export default Index
